bcfe1a4270540716faf797f4addb7309b0bdac6b
Michael Antony <Michael483711@outlook.com>
2024-11-14 13:02:41 +0000

    Added Two models

---
+++ Models/Project.cs
@@ -0,0 +1,13 @@
+﻿namespace PortfolioMVC.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Link { get; set; }
+    }
+}
+
+
